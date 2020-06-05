2237531b98cdee5bb9236666184deb6dbff2ab95
TsekotAndrey <61335008+TsekotAndrey@users.noreply.github.com>
2020-06-05 16:26:35 +0300

    fix1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.List;
 
 public class Main {
@@ -11,18 +10,11 @@ public class Main {
         ChromeDriver chromeDriver = new ChromeDriver();
         chromeDriver.manage().window().maximize();
         chromeDriver.get("https://brest.jobs.tut.by/");
-
-        chromeDriver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/div[1]/div/div/div/button/span")).click();
         chromeDriver.findElement(By.xpath("//span[contains(@class,'bloko-icon_highlighted-default')]")).click();
         chromeDriver.findElement(By.xpath("//input[@id='advancedsearchmainfield']")).sendKeys("QA");
-
-
         chromeDriver.findElement(By.xpath("//input[@id='advancedsearchmainfield']")).sendKeys(Keys.RETURN);
         chromeDriver.findElement(By.xpath("//span[@class='clusters-value__name clusters-value__name_truncate']")).click();
-
-
         List<WebElement> vacancy = chromeDriver.findElements(By.xpath("//div[@Class='vacancy-serp-item ']"));
-
         if (vacancy.size() >= 1) System.out.println("It is good day");
         else System.out.println("It is not good day");
         chromeDriver.close();
