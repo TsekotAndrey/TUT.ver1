b779429a03c27f69a565bd2a749d1b51072f1e6e
TsekotAndrey <61335008+TsekotAndrey@users.noreply.github.com>
2020-06-05 14:24:54 +0300

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,12 +11,19 @@ public class Main {
         ChromeDriver chromeDriver = new ChromeDriver();
         chromeDriver.manage().window().maximize();
         chromeDriver.get("https://brest.jobs.tut.by/");
-        chromeDriver.findElement(By.xpath("//a[contains(@class,'bloko-icon-link')]")).click();
+
+        chromeDriver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/div[1]/div/div/div/button/span")).click();
+        chromeDriver.findElement(By.xpath("//span[contains(@class,'bloko-icon_highlighted-default')]")).click();
         chromeDriver.findElement(By.xpath("//input[@id='advancedsearchmainfield']")).sendKeys("QA");
+
+
         chromeDriver.findElement(By.xpath("//input[@id='advancedsearchmainfield']")).sendKeys(Keys.RETURN);
-        List<WebElement> vacancy = chromeDriver.findElements(By.xpath("//div[@class='vacancy-serp']"));
-        //Косячный xPath на 17 строчке)))))))))
-        if (vacancy.size() > 1) System.out.println("It is good day");
+        chromeDriver.findElement(By.xpath("//span[@class='clusters-value__name clusters-value__name_truncate']")).click();
+
+
+        List<WebElement> vacancy = chromeDriver.findElements(By.xpath("//div[@Class='vacancy-serp-item ']"));
+
+        if (vacancy.size() >= 1) System.out.println("It is good day");
         else System.out.println("It is not good day");
         chromeDriver.close();
     }
